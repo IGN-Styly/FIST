8d91d2a4a9c20872e9bafce81d4530e4ef361a28
IGN-Styly <claudioresendeptbr@gmail.com>
2022-01-19 20:50:57 -0300

    Finished FIST interpreter and now back to TokenAPI

--- FIST.py
+++ FIST.py
@@ -1,6 +1,7 @@
 import json
 
-def is_valid(smc_type): #checks if smc_type is valid
+
+def is_valid(smc_type):  # checks if smc_type is valid
     if smc_type == 'vmt':
         return True
     elif smc_type == 'flt':
@@ -10,36 +11,59 @@ def is_valid(smc_type): #checks if smc_type is valid
     else:
         return False
 
+
 def parse_vmt(contract):
-    try: 
+    try:
         contract.get('value')
         contract.get('seller')
         contract.get('product')
     except:
         return False
     return True
+
 
 def parse_flt(contract):
-    try: 
+    try:
         contract.get('value')
         contract.get('seller')
         contract.get('product')
         contract.get('land-id')
-        contract.get('land-id')
+        contract.get('land-id').get('1')
     except:
         return False
     return True
 
+
+def parse_nfl(contract):
+    try:
+        contract.get('value')
+        contract.get('seller')
+        contract.get('product')
+        contract.get('land-id')
+        contract.get('land-id').get('1')
+    except:
+        return False
+    return True
+
+def parse_type(smc_type, contract):
+    if smc_type=='vmt':
+        return parse_vmt(contract)
+    elif smc_type=='flt':
+        return parse_flt(contract)
+    elif smc_type == 'nfl':
+        return parse_nfl(contract)
 
 def parse_contract(filename):
     try:
-        contract = json.load(open(filename)) # Tries to load the contract
+        contract = json.load(open(filename))  # Tries to load the contract
+        smc_type = contract.get('type')
     except:
         return False
 
-    if(is_valid(smc_type) is False): #validate if the contract is valid
+    if(is_valid(smc_type) is False):  # validate if the contract type is valid
         return print("Not a valid contract")
-    return contract
-
-
-parse_contract('vmt.json')
+    
+    if parse_type(smc_type=smc_type, contract=contract) is True:
+        return contract
+    else:
+        return False
